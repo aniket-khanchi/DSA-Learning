20ae3640631549bd49719fb6515fc8f8485298d5
Aniket Khanchi <aniket.khanchi@iu-study.org>
2022-08-31 01:08:21 +0530

    two recursive methods bottom_insert, reverse_stack

--- EX4_stack/stack_2nd.py
+++ EX4_stack/stack_2nd.py
@@ -1,3 +1,6 @@
+from inspect import stack
+
+
 class Stack():
     def __init__(self):
         self.items = []
@@ -16,4 +19,32 @@ class Stack():
             return self.items[-1]
         
     def get_stack(self):
-        return self.items
+        print(self.items)
+
+    # insert a value at bottom of stack using recursion
+    def insert_bottom(self,value):
+        if self.is_empty():
+            self.push(value)
+        else:
+            pop_val = self.pop()
+            self.insert_bottom(value)
+            self.push(pop_val)
+    #reverse a stack using recursion
+    def reverse_stack(self):
+        if self.is_empty():
+            pass
+        else:
+            pop_value = self.pop()
+            self.reverse_stack()
+            self.insert_bottom(pop_value) 
+
+if __name__ == "__main__":
+    s = Stack()
+    s.push(5)
+    s.push(7)
+
+    s.insert_bottom(0)
+    s.reverse_stack()
+    s.get_stack()
+   
+
