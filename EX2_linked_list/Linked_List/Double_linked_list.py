0dd20adaaef0d8543eea27ceb4af97b5ec69ac2c
Aniket <aniket.khanchi@iu-study.org>
2022-09-26 00:14:15 +0530

    26/09/2022 Add after/before node --Completed

--- EX2_linked_list/Linked_List/Double_linked_list.py
+++ EX2_linked_list/Linked_List/Double_linked_list.py
@@ -77,7 +77,62 @@ class DoublyLinkedList:
             cur = cur.next
     
     def add_node_after(self,index,data):
-        pass
+        cur = self.head
+        #previous = None
+        next_node = cur.next
+        node_index = 0
+        while cur:
+            if node_index == index:
+                if next_node == None:
+                    new_data = Node(data)
+                    cur.next = new_data
+                    new_data.prev = cur
+                    break                  
+                else:
+                    new_data = Node(data)
+                    next_node.prev = new_data
+                    new_data.prev = cur
+                    new_data.next = next_node
+                    cur.next = new_data
+                    break
+                
+            next_node = next_node.next 
+            cur = cur.next
+            
+            node_index += 1
+
+
+    def add_after_node(self, key, data):
+        cur = self.head
+        while cur:
+            if cur.next is None and cur.data == key:
+                self.append(data)
+                return
+            elif cur.data == key:
+                new_node = Node(data)
+                nxt = cur.next 
+                cur.next = new_node
+                new_node.next = nxt
+                new_node.prev = cur 
+                nxt.prev = new_node
+                return
+            cur = cur.next
+    
+    def add_before_node(self, key, data):
+        cur = self.head 
+        while cur:
+            if cur.prev is None and cur.data == key:
+                self.prepend(data)
+                return
+            elif cur.data == key:
+                new_node = Node(data)
+                prev = cur.prev
+                prev.next = new_node
+                cur.prev = new_node
+                new_node.next = cur
+                new_node.prev = prev
+                return 
+            cur = cur.next
             
             
                 
@@ -91,7 +146,7 @@ dll1.append(5)
 dll1.append(7)
 dll1.append(8)
 dll1.prepend(0)
-dll1.add_node_before_via_key(8,50)
+dll1.add_node_after(5,50)
 
 dll1.print_list()
 
