08d9cd8b30050bc5bd764e8ef85c07d1832a0d56
Aniket <aniket.khanchi@iu-study.org>
2022-09-28 23:38:14 +0530

    28/09/2022 Completed Advance Game in Array

--- EX1_array/Array/array.py
+++ EX1_array/Array/array.py
@@ -4,17 +4,33 @@ given that the maximum you can advance from a position is based on the
 value of the array at the index you are currently present on?
 """
 
-def array_game(l):
-    cur_pos = 0
-    while cur_pos < len(l) and l[cur_pos] > 0:
-        cur_pos = 1 + l[cur_pos]
-        cur_pos = l[cur_pos]
-        print(cur_pos)
-        if cur_pos == len(l) -1:
-            print('yeah')
+# def array_advance(A):
+#     furthest_reached = 0
+#     last_idx = len(A) - 1
+#     i = 0
+#     while i <= furthest_reached and furthest_reached < last_idx:
+#         furthest_reached = max(furthest_reached, A[i] + i)
+#         i += 1
+#     return furthest_reached >= last_idx
+
+def array_advance(A):
+    idx = 0
+    val = None
+    while idx < len(A) - 1 and val != 0:
+        val = A[idx]
+        idx = idx + val
+    if idx == len(A) -1:
+        print('success')
+    else:
+        print('Failure!')
+   
 
 
-x = [1,3,1,0,2,0,1]
-array_game(x)
+
+
+
+# x = [1,3,1,0,2,0,1]
+x =[3, 2, 0, 0, 2, 0, 1]
+array_advance(x)
 
 
