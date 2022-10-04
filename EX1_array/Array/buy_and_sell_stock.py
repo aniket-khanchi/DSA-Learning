951e9a673ef2920cec88967560e4cc8b8486f7c1
Aniket <aniket.khanchi@iu-study.org>
2022-10-05 00:02:01 +0530

    05/10/2022 buy and sell stock -- Completed

--- EX1_array/Array/buy_and_sell_stock.py
+++ EX1_array/Array/buy_and_sell_stock.py
@@ -7,3 +7,14 @@ In an array of prices, each index represents a day, and the value
 on that index represents the price of the stocks on that day.
 """
 
+def buy_and_sell_stock_once(prices):
+    max_profit = 0
+    min_price = float('inf')
+    for i in prices:
+        min_price = min(i,min_price)
+        compare = i - min_price
+        max_profit = max(max_profit,compare)
+    return max_profit
+
+
+print(buy_and_sell_stock_once([310, 315, 275, 295, 260, 270, 290, 230, 255, 250]))
