89f7fa35173bb6808ca204ed24ff9f4f28ab605a
Aniket <aniket.khanchi@iu-study.org>
2022-11-12 00:34:09 +0530

    challenging day stuck in recursion

--- Recursion/w3resource.py
+++ Recursion/w3resource.py
@@ -1,13 +1,24 @@
 # 1. Write a Python program to calculate the sum of a list of numbers.
-x = [1,3,4,5,6]
+# x = [1,3,4,5,6]
 
-def sum_list(ls,idx):
-    if idx == len(ls):
-        return 0  
-    sum = ls[idx]
-    return sum + sum_list(ls,idx+1)
+# def sum_list(ls,idx):
+#     if idx == len(ls):
+#         return 0  
+#     sum = ls[idx]
+#     return sum + sum_list(ls,idx+1)
 
-print(sum_list(x,0))
+# print(sum_list(x,0))
 
 
 # 2. Write a Python program to converting an Integer to a string in any base.
+
+def to_string(n,base):
+   conver_tString = "0123456789ABCDEF"
+   if n < base:
+      return conver_tString[n]
+   else:
+      return to_string(n//base,base) + conver_tString[n % base]
+
+print(to_string(2835,16))
+
+#
